323471b044fa0478080923a0c870949a113d5de2
Robert Wallström <robert@smithimage.com>
2017-04-08 02:08:55 +0200

    Add description to assembly

--- src/Akka.Persistence.AzureTable/Properties/AssemblyInfo.cs
+++ src/Akka.Persistence.AzureTable/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Akka.Persistence.Azure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Akka Persistence journal and snapshot store backed by Azure Table database.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Akka.Persistence.Azure")]
 [assembly: AssemblyCulture("")]
